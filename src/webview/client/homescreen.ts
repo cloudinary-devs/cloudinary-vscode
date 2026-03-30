cb1e1c4eb58d2ee34e33cda140673a4cad2e3c31
Nick Bradley <nick.bradley@cloudinary.com>
2026-03-30 13:50:05 +0100

    feat: add homescreen client script

---
+++ src/webview/client/homescreen.ts
@@ -0,0 +1,44 @@
+/**
+ * Homescreen webview client-side script.
+ * Handles button actions by posting messages to the extension host.
+ */
+
+import { initVSCode, getVSCode } from "./common";
+
+function openGlobalConfig(): void {
+  getVSCode()?.postMessage({ command: "openGlobalConfig" });
+}
+
+function showLibrary(): void {
+  getVSCode()?.postMessage({ command: "showLibrary" });
+}
+
+function openUploadWidget(): void {
+  getVSCode()?.postMessage({ command: "openUploadWidget" });
+}
+
+function searchAssets(): void {
+  getVSCode()?.postMessage({ command: "searchAssets" });
+}
+
+function openWelcomeScreen(): void {
+  getVSCode()?.postMessage({ command: "openWelcomeScreen" });
+}
+
+declare global {
+  interface Window {
+    openGlobalConfig: typeof openGlobalConfig;
+    showLibrary: typeof showLibrary;
+    openUploadWidget: typeof openUploadWidget;
+    searchAssets: typeof searchAssets;
+    openWelcomeScreen: typeof openWelcomeScreen;
+  }
+}
+
+window.openGlobalConfig = openGlobalConfig;
+window.showLibrary = showLibrary;
+window.openUploadWidget = openUploadWidget;
+window.searchAssets = searchAssets;
+window.openWelcomeScreen = openWelcomeScreen;
+
+initVSCode();
